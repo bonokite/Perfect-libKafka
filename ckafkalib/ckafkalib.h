92778ad0df6be33d5895f2789414c25be3ecf05f
Adolfo Encinas Martín <adolfo.encinas@me.com>
2018-10-30 12:12:43 +0100

    ADD - Xcode project & shared scheme

---
+++ ckafkalib/ckafkalib.h
@@ -0,0 +1,18 @@
+//
+//  ckafkalib.h
+//  ckafkalib
+//
+//  Created by Adolfo on 30/10/2018.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ckafkalib.
+FOUNDATION_EXPORT double ckafkalibVersionNumber;
+
+//! Project version string for ckafkalib.
+FOUNDATION_EXPORT const unsigned char ckafkalibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ckafkalib/PublicHeader.h>
+#include "/usr/local/include/librdkafka/rdkafka.h"
+

ec7d0028f0dd1a810bc5e6a25aa7b47645634a9b
Adolfo Encinas Martín <adolfo.encinas@me.com>
2018-10-30 12:59:01 +0100

    v1.0.5

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "ckafka",
+    products: [
+        .library(
+            name: "ckafka",
+            targets: ["ckafka"]
+        )
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
